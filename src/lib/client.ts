50a515ef9726c311b0ecb775d808cb667e264e4c
CapablancaYEHA <lulushe4ka@gmail.com>
2023-12-17 22:34:51 +0300

    fix

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const isDev = () => process.env.NODE_ENV === "development";
-const url = isDev() ? "http://localhost:4000/api" : "http://89.223.31.81:4000";
+const url = isDev()
+  ? "http://localhost:4000/api"
+  : "http://89.223.31.81:4000/api";
 
 const instance = axios.create({
   baseURL: url,
